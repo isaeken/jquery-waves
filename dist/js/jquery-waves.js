97dcb3f5e1602153dc55d6545dfdd44aa7e976ce
isaeken <hello@isaeken.com.tr>
2020-11-25 12:37:14 +0300

    add comments

--- dist/js/jquery-waves.js
+++ dist/js/jquery-waves.js
@@ -30,6 +30,13 @@ if (!window.jQuery) {
 
     return color;
   }
+  /**
+   * get data from element if is exists else returns default value
+   * @param key
+   * @param defaultValue
+   * @returns {jQuery|*|null}
+   */
+
 
   $.fn.getElementData = function (key) {
     var defaultValue = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
@@ -59,26 +66,65 @@ if (!window.jQuery) {
     var opacity = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
     var duration = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 800;
     var onComplete = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : function () {};
+
+    /**
+     * the target element
+     * @type {jQuery.fn.init|jQuery|HTMLElement}
+     */
     var $this = $(this);
+    /**
+     * wave ripple element
+     * @type {jQuery.fn.init|jQuery|HTMLElement}
+     */
+
     var $effect = $('<div class="ie-waves-effect"></div>');
+    /**
+     * original css position value
+     * @type {jQuery}
+     */
+
     var originalPosition = $this.css('position');
+    /**
+     * original css overflow value
+     * @type {jQuery}
+     */
+
     var originalOverflow = $this.css('overflow');
+    /**
+     * set y position if its lower than 0
+     */
 
     if (positionY < 0) {
       positionY = $this.innerWidth() / 2;
     }
+    /**
+     * set x position if its lower than 0
+     */
+
 
     if (positionX < 0) {
       positionX = $(this).innerHeight() / 2;
     }
+    /**
+     * set background color is random if its not null and 'RANDOM'
+     */
+
 
     if (backgroundColor != null && backgroundColor.toUpperCase().trim() === 'RANDOM') {
       backgroundColor = randomColor();
     }
+    /**
+     * set ripple opacity '0.15' if its null
+     */
+
 
     if (opacity === null) {
       opacity = '0.15';
     }
+    /**
+     * apply styles to ripple
+     */
+
 
     $effect.css({
       top: positionY,
@@ -86,42 +132,124 @@ if (!window.jQuery) {
       backgroundColor: backgroundColor,
       opacity: opacity
     });
+    /**
+     * add ripple to target
+     */
+
     $this.append($effect);
+    /**
+     * change target styles
+     */
+
     $this.css({
       overflow: 'hidden',
       position: 'relative'
     });
-    var size = $(this).outerWidth();
+    /**
+     * ripple width
+     * @type {*|jQuery}
+     */
+
+    var size = $this.outerWidth();
+    /**
+     * set size is target height if target width lower than height
+     */
 
     if ($this.outerWidth() < $this.outerHeight()) {
       size = $this.outerHeight();
     }
+    /**
+     * set size to complete size
+     * @type {number}
+     */
+
+
+    size = size * 2 + 50;
+    /**
+     * animate ripple
+     */
 
     $effect.animate({
-      width: size * 2 + 50 + 'px',
-      height: size * 2 + 50 + 'px',
+      width: size + 'px',
+      height: size + 'px',
       opacity: 0
     }, duration, function () {
+      /**
+       * remove ripple after animation complete
+       */
       $effect.remove();
+      /**
+       * set target default styles after animation complete
+       */
+
       $this.css({
         position: originalPosition,
         overflow: originalOverflow
       });
+      /**
+       * call handler after animation complete
+       */
+
       onComplete();
     });
     return this;
   };
 })(jQuery);
+/**
+ * document on ready
+ */
+
 
 $(document).ready(function () {
+  /**
+   * on .waves-effect and [data-waves] click event
+   */
   $('.waves-effect, [data-waves]').click(function (event) {
+    /**
+     * the target element
+     * @type {*|jQuery.fn.init|jQuery|HTMLElement}
+     */
     var $this = $(this);
+    /**
+     * ripple background color from data
+     * @type {jQuery|jQuery|*|null}
+     */
+
     var backgroundColor = $this.getElementData('waves-background-color', null);
+    /**
+     * ripple opacity from data
+     * @type {jQuery|jQuery|*|null}
+     */
+
     var opacity = $this.getElementData('waves-opacity', null);
+    /**
+     * ripple animation duration from data
+     * @type {number}
+     */
+
     var duration = parseInt($this.getElementData('waves-duration', 800));
+    /**
+     * ripple complete event from data
+     * @type {jQuery|jQuery|*|null}
+     */
+
     var onComplete = $this.getElementData('waves-on-complete', function () {});
+    /**
+     * calculate clicked position X
+     * @type {number}
+     */
+
     var positionX = event.pageX - this.offsetLeft;
+    /**
+     * calculate clicked position Y
+     * @type {number}
+     */
+
     var positionY = event.pageY - this.offsetTop;
+    /**
+     * execute ripple effect to target
+     */
+
     $this.waveEffect(positionX, positionY, backgroundColor, opacity, duration, onComplete);
   });
 });
--- src/js/jquery-waves.js
+++ src/js/jquery-waves.js
@@ -27,6 +27,12 @@ if (!window.jQuery) {
         return color;
     }
 
+    /**
+     * get data from element if is exists else returns default value
+     * @param key
+     * @param defaultValue
+     * @returns {jQuery|*|null}
+     */
     $.fn.getElementData = function (key, defaultValue = null) {
         const $this = $(this);
         let exists = $this[0].hasAttribute('data-' + key);
@@ -47,27 +53,61 @@ if (!window.jQuery) {
      * @returns {jQuery}
      */
     $.fn.waveEffect = function (positionX, positionY, backgroundColor = null, opacity = null, duration = 800, onComplete = function () {}) {
+        /**
+         * the target element
+         * @type {jQuery.fn.init|jQuery|HTMLElement}
+         */
         const $this = $(this);
+
+        /**
+         * wave ripple element
+         * @type {jQuery.fn.init|jQuery|HTMLElement}
+         */
         const $effect = $('<div class="ie-waves-effect"></div>');
+
+        /**
+         * original css position value
+         * @type {jQuery}
+         */
         let originalPosition = $this.css('position');
+
+        /**
+         * original css overflow value
+         * @type {jQuery}
+         */
         let originalOverflow = $this.css('overflow');
 
+        /**
+         * set y position if its lower than 0
+         */
         if (positionY < 0) {
             positionY = $this.innerWidth() / 2;
         }
 
+        /**
+         * set x position if its lower than 0
+         */
         if (positionX < 0) {
             positionX = $(this).innerHeight() / 2;
         }
 
+        /**
+         * set background color is random if its not null and 'RANDOM'
+         */
         if (backgroundColor != null && backgroundColor.toUpperCase().trim() === 'RANDOM') {
             backgroundColor = randomColor();
         }
 
+        /**
+         * set ripple opacity '0.15' if its null
+         */
         if (opacity === null) {
             opacity = '0.15';
         }
 
+        /**
+         * apply styles to ripple
+         */
         $effect.css({
             top: positionY,
             left: positionX,
@@ -75,28 +115,62 @@ if (!window.jQuery) {
             opacity: opacity,
         });
 
+        /**
+         * add ripple to target
+         */
         $this.append($effect);
+
+        /**
+         * change target styles
+         */
         $this.css({
             overflow: 'hidden',
             position: 'relative',
         });
 
-        let size = $(this).outerWidth();
+        /**
+         * ripple width
+         * @type {*|jQuery}
+         */
+        let size = $this.outerWidth();
 
+        /**
+         * set size is target height if target width lower than height
+         */
         if ($this.outerWidth() < $this.outerHeight()) {
             size = $this.outerHeight();
         }
 
+        /**
+         * set size to complete size
+         * @type {number}
+         */
+        size = size * 2 + 50;
+
+        /**
+         * animate ripple
+         */
         $effect.animate({
-            width: (size * 2 + 50) + 'px',
-            height: (size * 2 + 50) + 'px',
+            width: size + 'px',
+            height: size + 'px',
             opacity: 0,
         }, duration, function () {
+            /**
+             * remove ripple after animation complete
+             */
             $effect.remove();
+
+            /**
+             * set target default styles after animation complete
+             */
             $this.css({
                 position: originalPosition,
                 overflow: originalOverflow,
             });
+
+            /**
+             * call handler after animation complete
+             */
             onComplete();
         });
 
@@ -104,17 +178,59 @@ if (!window.jQuery) {
     };
 }) (jQuery);
 
+/**
+ * document on ready
+ */
 $(document).ready(() => {
+    /**
+     * on .waves-effect and [data-waves] click event
+     */
     $('.waves-effect, [data-waves]').click(function (event) {
+        /**
+         * the target element
+         * @type {*|jQuery.fn.init|jQuery|HTMLElement}
+         */
         const $this = $(this);
+
+        /**
+         * ripple background color from data
+         * @type {jQuery|jQuery|*|null}
+         */
         let backgroundColor = $this.getElementData('waves-background-color', null);
+
+        /**
+         * ripple opacity from data
+         * @type {jQuery|jQuery|*|null}
+         */
         let opacity = $this.getElementData('waves-opacity', null);
+
+        /**
+         * ripple animation duration from data
+         * @type {number}
+         */
         let duration = parseInt($this.getElementData('waves-duration', 800));
+
+        /**
+         * ripple complete event from data
+         * @type {jQuery|jQuery|*|null}
+         */
         let onComplete = $this.getElementData('waves-on-complete', function () {});
 
+        /**
+         * calculate clicked position X
+         * @type {number}
+         */
         let positionX = event.pageX - this.offsetLeft;
+
+        /**
+         * calculate clicked position Y
+         * @type {number}
+         */
         let positionY = event.pageY - this.offsetTop;
 
+        /**
+         * execute ripple effect to target
+         */
         $this.waveEffect(positionX, positionY, backgroundColor, opacity, duration, onComplete);
     });
 });
