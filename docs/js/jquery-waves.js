096d3a48cd9fbb7ae4c213ddb4072c24f6294f84
isaeken <hello@isaeken.com.tr>
2020-11-25 12:25:31 +0300

    optimized code

--- docs/js/jquery-waves.js
+++ docs/js/jquery-waves.js
@@ -1,75 +1,127 @@
 "use strict";
 
+// jquery-waves 1.1.0
+// (c) 2020 İsa Eken
+// jquery-waves may be freely distributed under the MIT license.
+// for all details and documentation:
+// https://github.com/isaeken/jquery-waves
+
 /**
- *     @author İsa Eken <hello@isaeken.com.tr>
- *     @website https://isaeken.com.tr
- *     @github https://github.com/isaeken
- *     @name jquery.waves
+ * throw an error if jquery if not initialized
  */
-if (!window.jQuery) console.error('Waves required jQuery.');else {
-  (function ($) {
-    $.fn._wave = function (x, y) {
-      var _color = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
-
-      var _opacity = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
-
-      var _duration = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 800;
-
-      var _handler = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : function () {};
-
-      if (!$(this).hasClass('waves-effect')) console.error('waves class required.');else {
-        var $wave = $('<div class="ie-waves-effect"></div>');
-        if (y < 0) y = $(this).innerWidth() / 2;
-        if (x < 0) x = $(this).innerHeight() / 2;
-
-        if (_color === 'RANDOM') {
-          var letters = '0123456789ABCDEF';
-          var color = '#';
-
-          for (var i = 0; i < 6; i++) {
-            color += letters[Math.floor(Math.random() * 16)];
-          }
-
-          _color = color;
-        }
-
-        $wave.css('top', y).css('left', x);
-        if (_color !== null) $wave.css('background-color', _color);
-        if (_opacity !== null) $wave.css('opacity', _opacity);
-        $(this).append($wave);
-        var wave_size = $(this).outerWidth();
-        if ($(this).outerWidth() < $(this).outerHeight()) wave_size = $(this).outerHeight();
-        $wave.animate({
-          width: wave_size * 2 + 50 + 'px',
-          height: wave_size * 2 + 50 + 'px',
-          opacity: 0
-        }, _duration, function () {
-          $wave.remove();
-
-          _handler();
-        });
-      }
-      return this;
-    };
-  })(jQuery);
-
-  $(document).ready(function () {
-    $('.waves-effect').each(function () {
-      $(this).click(function (event) {
-        var _color = null;
-        var _opacity = null;
-        var _duration = 800;
-
-        var _handler = function _handler() {};
-
-        if ($(this).hasClass('waves-rainbow')) _color = 'RANDOM';
-        if ($(this).hasClass('waves-fast')) _duration = 500;
-        if ($(this).hasClass('waves-slow')) _duration = 1800;
-        if ($(this).attr('waves-color') !== undefined) _color = $(this).attr('waves-color');
-        if ($(this).attr('waves-duration') !== undefined) _duration = parseInt($(this).attr('waves-duration'));
-
-        $(this)._wave(event.pageX - this.offsetLeft, event.pageY - this.offsetTop, _color, _opacity, _duration, _handler);
-      });
-    });
-  });
+if (!window.jQuery) {
+  throw 'jquery-waves is requires jquery!';
 }
+
+(function ($) {
+  'use strict';
+  /**
+   * create random color
+   * @returns {string}
+   */
+
+  function randomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+
+    return color;
+  }
+
+  $.fn.getElementData = function (key) {
+    var defaultValue = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : null;
+    var $this = $(this);
+    var exists = $this[0].hasAttribute('data-' + key);
+
+    if (!exists) {
+      return defaultValue;
+    }
+
+    return $this.data(key);
+  };
+  /**
+   * create wave effect to element
+   * @param positionX
+   * @param positionY
+   * @param backgroundColor
+   * @param opacity
+   * @param duration
+   * @param onComplete
+   * @returns {jQuery}
+   */
+
+
+  $.fn.waveEffect = function (positionX, positionY) {
+    var backgroundColor = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : null;
+    var opacity = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : null;
+    var duration = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : 800;
+    var onComplete = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : function () {};
+    var $this = $(this);
+    var $effect = $('<div class="ie-waves-effect"></div>');
+    var originalPosition = $this.css('position');
+    var originalOverflow = $this.css('overflow');
+
+    if (positionY < 0) {
+      positionY = $this.innerWidth() / 2;
+    }
+
+    if (positionX < 0) {
+      positionX = $(this).innerHeight() / 2;
+    }
+
+    if (backgroundColor != null && backgroundColor.toUpperCase().trim() === 'RANDOM') {
+      backgroundColor = randomColor();
+    }
+
+    if (opacity === null) {
+      opacity = '0.15';
+    }
+
+    $effect.css({
+      top: positionY,
+      left: positionX,
+      backgroundColor: backgroundColor,
+      opacity: opacity
+    });
+    $this.append($effect);
+    $this.css({
+      overflow: 'hidden',
+      position: 'relative'
+    });
+    var size = $(this).outerWidth();
+
+    if ($this.outerWidth() < $this.outerHeight()) {
+      size = $this.outerHeight();
+    }
+
+    $effect.animate({
+      width: size * 2 + 50 + 'px',
+      height: size * 2 + 50 + 'px',
+      opacity: 0
+    }, duration, function () {
+      $effect.remove();
+      $this.css({
+        position: originalPosition,
+        overflow: originalOverflow
+      });
+      onComplete();
+    });
+    return this;
+  };
+})(jQuery);
+
+$(document).ready(function () {
+  $('.waves-effect, [data-waves]').click(function (event) {
+    var $this = $(this);
+    var backgroundColor = $this.getElementData('waves-background-color', null);
+    var opacity = $this.getElementData('waves-opacity', null);
+    var duration = parseInt($this.getElementData('waves-duration', 800));
+    var onComplete = $this.getElementData('waves-on-complete', function () {});
+    var positionX = event.pageX - this.offsetLeft;
+    var positionY = event.pageY - this.offsetTop;
+    $this.waveEffect(positionX, positionY, backgroundColor, opacity, duration, onComplete);
+  });
+});
--- src/js/jquery-waves.js
+++ src/js/jquery-waves.js
@@ -4,75 +4,117 @@
 // for all details and documentation:
 // https://github.com/isaeken/jquery-waves
 
-if (!window.jQuery) console.error('Waves required jQuery.');
-else
-{
-    (function ($) {
-        $.fn._wave = function (x, y, _color = null, _opacity = null, _duration = 800, _handler = function () {}) {
-            if (!$(this).hasClass('waves-effect')) console.error('waves class required.');
-            else
-            {
-                var $wave = $('<div class="ie-waves-effect"></div>');
-
-                if (y < 0) y = $(this).innerWidth() / 2;
-                if (x < 0) x = $(this).innerHeight() / 2;
-
-                if (_color === 'RANDOM') {
-                    var letters = '0123456789ABCDEF';
-                    var color = '#';
-                    for (var i = 0; i < 6; i++) color += letters[Math.floor(Math.random() * 16)];
-                    _color = color;
-                }
-
-                $wave
-                    .css('top', y)
-                    .css('left', x);
-
-                if (_color !== null) $wave.css('background-color', _color);
-                if (_opacity !== null) $wave.css('opacity', _opacity);
-
-                $(this).append($wave);
-
-                var wave_size = $(this).outerWidth();
-                if ($(this).outerWidth() < $(this).outerHeight())
-                    wave_size = $(this).outerHeight();
-
-                $wave.animate({
-                    width: (wave_size * 2 + 50) + 'px',
-                    height: (wave_size * 2 + 50) + 'px',
-                    opacity: 0,
-                }, _duration, function () {
-                    $wave.remove();
-                    _handler();
-                });
-            }
-            return this;
-        };
-    }) (jQuery);
-
-    $(document).ready(() => {
-        $('.waves-effect').each(function () {
-            $(this).click(function (event) {
-                var _color = null;
-                var _opacity = null;
-                var _duration = 800;
-                var _handler = function () { };
-
-                if ($(this).hasClass('waves-rainbow')) _color = 'RANDOM';
-                if ($(this).hasClass('waves-fast')) _duration = 500;
-                if ($(this).hasClass('waves-slow')) _duration = 1800;
-                if ($(this).attr('waves-color') !== undefined) _color = $(this).attr('waves-color');
-                if ($(this).attr('waves-duration') !== undefined) _duration = parseInt($(this).attr('waves-duration'));
-
-                $(this)._wave(
-                    event.pageX - this.offsetLeft,
-                    event.pageY - this.offsetTop,
-                    _color,
-                    _opacity,
-                    _duration,
-                    _handler
-                );
-            });
-        });
-    });
+/**
+ * throw an error if jquery if not initialized
+ */
+if (!window.jQuery) {
+    throw 'jquery-waves is requires jquery!';
 }
+
+(function ($) {
+    'use strict';
+
+    /**
+     * create random color
+     * @returns {string}
+     */
+    function randomColor() {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+
+    $.fn.getElementData = function (key, defaultValue = null) {
+        const $this = $(this);
+        let exists = $this[0].hasAttribute('data-' + key);
+        if (!exists) {
+            return defaultValue;
+        }
+        return $this.data(key);
+    };
+
+    /**
+     * create wave effect to element
+     * @param positionX
+     * @param positionY
+     * @param backgroundColor
+     * @param opacity
+     * @param duration
+     * @param onComplete
+     * @returns {jQuery}
+     */
+    $.fn.waveEffect = function (positionX, positionY, backgroundColor = null, opacity = null, duration = 800, onComplete = function () {}) {
+        const $this = $(this);
+        const $effect = $('<div class="ie-waves-effect"></div>');
+        let originalPosition = $this.css('position');
+        let originalOverflow = $this.css('overflow');
+
+        if (positionY < 0) {
+            positionY = $this.innerWidth() / 2;
+        }
+
+        if (positionX < 0) {
+            positionX = $(this).innerHeight() / 2;
+        }
+
+        if (backgroundColor != null && backgroundColor.toUpperCase().trim() === 'RANDOM') {
+            backgroundColor = randomColor();
+        }
+
+        if (opacity === null) {
+            opacity = '0.15';
+        }
+
+        $effect.css({
+            top: positionY,
+            left: positionX,
+            backgroundColor: backgroundColor,
+            opacity: opacity,
+        });
+
+        $this.append($effect);
+        $this.css({
+            overflow: 'hidden',
+            position: 'relative',
+        });
+
+        let size = $(this).outerWidth();
+
+        if ($this.outerWidth() < $this.outerHeight()) {
+            size = $this.outerHeight();
+        }
+
+        $effect.animate({
+            width: (size * 2 + 50) + 'px',
+            height: (size * 2 + 50) + 'px',
+            opacity: 0,
+        }, duration, function () {
+            $effect.remove();
+            $this.css({
+                position: originalPosition,
+                overflow: originalOverflow,
+            });
+            onComplete();
+        });
+
+        return this;
+    };
+}) (jQuery);
+
+$(document).ready(() => {
+    $('.waves-effect, [data-waves]').click(function (event) {
+        const $this = $(this);
+        let backgroundColor = $this.getElementData('waves-background-color', null);
+        let opacity = $this.getElementData('waves-opacity', null);
+        let duration = parseInt($this.getElementData('waves-duration', 800));
+        let onComplete = $this.getElementData('waves-on-complete', function () {});
+
+        let positionX = event.pageX - this.offsetLeft;
+        let positionY = event.pageY - this.offsetTop;
+
+        $this.waveEffect(positionX, positionY, backgroundColor, opacity, duration, onComplete);
+    });
+});
